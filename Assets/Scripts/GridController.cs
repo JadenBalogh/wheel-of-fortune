6b4fd397e887fc6b84fc84e29ddea9d832712f2b
Jaden Balogh <jadenbalogh@gmail.com>
2020-01-04 12:00:27 -0800

    Automate reading game layouts from file

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -3,16 +3,18 @@ using UnityEngine.UI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 public class GridController : MonoBehaviour {
     
     public int rows = 4;
     public int columns = 14;
-    public int gameToPlay;
+    public int gameToPlay = 0;
     public float xSeparation = 1.11f;
     public float ySeparation = 1.51f;
     public float xStart = -5f;
     public float yStart = -0.2f;
+    public string gameLayoutFilePath = "Assets/GameLayouts/Game1.layout";
     public AudioClip incorrectGuessSound;
     public AudioClip correctGuessSound;
     public AudioClip puzzleRevealedSound;
@@ -31,14 +33,14 @@ public class GridController : MonoBehaviour {
     public class GameLayout
     {
         public String category;
-        public String[] layout;
+        public char[] answer;
     }
 
     public void ResetGame()
     {
         GameLayout game = gameLayouts[gameToPlay];
 
-        for (int i = 0; i < game.layout.Length; i++)
+        for (int i = 0; i < game.answer.Length; i++)
         {
             Destroy(letterTiles[i]);
         }
@@ -64,14 +66,170 @@ public class GridController : MonoBehaviour {
         CreateGame(letterTilePrefab);
     }
 
-    void Start () {
-
+    void Start () 
+    {
         coverTiles = new List<GameObject>();
         letterTiles = new List<GameObject>();
+        ReadLayoutsFromFile();
         SetGridPositions();
         SetCategory();
         CreateGrid(coverTilePrefab);
         CreateGame(letterTilePrefab);
+    }
+
+    void ReadLayoutsFromFile()
+    {
+        StreamReader reader = new StreamReader(gameLayoutFilePath);
+        string result = reader.ReadToEnd();
+
+        char[] separator = { '[', ']' };
+        string[] split = result.Split(separator);
+
+        List<GameLayout> layouts = new List<GameLayout>();
+        GameLayout layout = new GameLayout();
+        bool isCategory = false;
+        bool isAnswer = false;
+        bool isLayout = false;
+
+        for (int i = 0; i < split.Length; i++)
+        {
+            string tmp = split[i].Trim();
+            switch(tmp)
+            {
+                case "GameLayout":
+                {
+                    layout = new GameLayout();
+                    isLayout = true;
+                    isCategory = false;
+                    isAnswer = false;
+                    break;
+                }
+                case "Category":
+                {
+                    isCategory = true;
+                    isAnswer = false;
+                    break;
+                }
+                case "Answer":
+                {
+                    isCategory = false;
+                    isAnswer = true;
+                    break;
+                }
+                case "":
+                {
+                    if (isLayout)
+                    {
+                        layouts.Add(layout);
+                        isLayout = false;
+                    }
+                    break;
+                }
+                default:
+                {
+                    if (isCategory)
+                    {
+                        layout.category = tmp.ToUpper();
+                    }
+                    if (isAnswer)
+                    {
+                        layout.answer = FitAnswerToDisplay(tmp.ToUpper());
+                    }
+                    break;
+                }
+            }
+        }
+
+        gameLayouts = layouts.ToArray();
+    }
+
+    char[] FitAnswerToDisplay(string answer)
+    {
+        // Assume each word fits on one line
+        // and the phrase fits on the display
+        string result = "";
+
+        string[] words = answer.Split(' ');
+        int index = 0;
+
+        int count = 0;
+        string line = " ";
+        while (index < words.Length && count + words[index].Length <= 12)
+        {
+            line += words[index];
+            count += words[index].Length;
+            index++;
+            if (index < words.Length && count + words[index].Length <= 12)
+            {
+                line += " ";
+                count++;
+            }
+        }
+        result += line.PadRight(14);
+
+        count = 0;
+        line = "";
+        while (index < words.Length && count + words[index].Length <= 14)
+        {
+            line += words[index];
+            count += words[index].Length;
+            index++;
+            if (index < words.Length && count + words[index].Length <= 14)
+            {
+                line += " ";
+                count++;
+            }
+        }
+        if (count < 14)
+        {
+            line = " " + line;
+        }
+        result += line.PadRight(14);
+
+        // try and center it
+        if (index == words.Length)
+        {
+            result = result.PadLeft(42, ' ');
+            result = result.PadRight(56, ' ');
+        }
+        else
+        {
+            count = 0;
+            line = "";
+            while (index < words.Length && count + words[index].Length <= 14)
+            {
+                line += words[index];
+                count += words[index].Length;
+                index++;
+                if (index < words.Length && count + words[index].Length <= 14)
+                {
+                    line += " ";
+                    count++;
+                }
+            }
+            if (count < 14)
+            {
+                line = " " + line;
+            }
+            result += line.PadRight(14);
+
+            count = 0;
+            line = " ";
+            while (index < words.Length && count + words[index].Length <= 12)
+            {
+                line += words[index];
+                count += words[index].Length;
+                index++;
+                if (index < words.Length && count + words[index].Length <= 12)
+                {
+                    line += " ";
+                    count++;
+                }
+            }
+            result += line.PadRight(14);
+        }
+
+        return result.ToCharArray();
     }
 
     void SetGridPositions()
@@ -83,9 +241,9 @@ public class GridController : MonoBehaviour {
 
         int count = 0;
 
-        for (int x = 0; x < columns; x++)
+        for (int y = 0; y < rows; y++)
         {
-            for (int y = 0; y < rows; y++)
+            for (int x = 0; x < columns; x++)
             {
                 if ((x == 0 && y == 0) || (x == 13 && y == 0) || (x == 0 && y == 3) || (x == 13 && y == 3))
                 {
@@ -96,12 +254,12 @@ public class GridController : MonoBehaviour {
                     gridPositions[count] = new Vector3(xPos, yPos, 0.0f);
                 }
 
-                yPos += ySeparation;
+                xPos += xSeparation;
                 count++;
             }
 
-            yPos = yStart;
-            xPos += xSeparation;
+            xPos = xStart;
+            yPos -= ySeparation;
         }
     }
 
@@ -143,20 +301,39 @@ public class GridController : MonoBehaviour {
     {
         GameLayout game = gameLayouts[gameToPlay];
 
-        for (int i = 0; i < game.layout.Length; i++)
+        for (int i = 0; i < game.answer.Length; i++)
         {
-            if (!game.layout[i].Equals(""))
+            if (game.answer[i] != ' ')
             {
                 Vector3 pos = gridPositions[i];
                 pos += new Vector3(0, 0, -1);
                 GameObject tile = Instantiate(obj, pos, Quaternion.identity) as GameObject;
                 letterTiles.Add(tile);
                 tile.GetComponentInChildren<MeshRenderer>().enabled = false;
-                tile.GetComponentInChildren<TextMesh>().text = game.layout[i];
+                tile.GetComponentInChildren<TextMesh>().text = game.answer[i].ToString();
             }
             else
             {
                 letterTiles.Add(null);
+            }
+        }
+
+        InitShownLetters();
+    }
+
+    void InitShownLetters()
+    {
+        GameLayout game = gameLayouts[gameToPlay];
+
+        for (int i = 0; i < game.answer.Length; i++)
+        {
+            if (game.answer[i] == '\'' ||
+                game.answer[i] == ':' ||
+                game.answer[i] == '?' ||
+                game.answer[i] == '&' ||
+                game.answer[i] == '-')
+            {
+                letterTiles[i].GetComponentInChildren<MeshRenderer>().enabled = true;
             }
         }
     }
@@ -170,11 +347,16 @@ public class GridController : MonoBehaviour {
 
         GameLayout game = gameLayouts[gameToPlay];
         
-        for (int i = 0; i < game.layout.Length; i++)
+        for (int i = 0; i < game.answer.Length; i++)
         {
-            if (!game.layout[i].Equals(""))
+            if (game.answer[i] != ' ' && 
+                game.answer[i] != '\'' && 
+                game.answer[i] != ':' && 
+                game.answer[i] != '?' && 
+                game.answer[i] != '&' && 
+                game.answer[i] != '-')
             {
-                if (Input.GetButtonDown(game.layout[i]))
+                if (Input.GetButtonDown(game.answer[i].ToString()))
                 {
                     letterTiles[i].GetComponentInChildren<MeshRenderer>().enabled = true;
                     
@@ -215,9 +397,9 @@ public class GridController : MonoBehaviour {
     {
         GameLayout game = gameLayouts[gameToPlay];
 
-        for (int i = 0; i < game.layout.Length; i++)
+        for (int i = 0; i < game.answer.Length; i++)
         {
-            if (game.layout[i].Equals(letterDisplays[index].GetComponentInChildren<Text>().text))
+            if (game.answer[i] == letterDisplays[index].GetComponentInChildren<Text>().text[0])
             {
                 return true;
             }
@@ -232,9 +414,9 @@ public class GridController : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Equals))
         {
-            for (int i = 0; i < game.layout.Length; i++)
+            for (int i = 0; i < game.answer.Length; i++)
             {
-                if (!game.layout[i].Equals(""))
+                if (game.answer[i] != ' ')
                 {
                     letterTiles[i].GetComponentInChildren<MeshRenderer>().enabled = true;
                 }
